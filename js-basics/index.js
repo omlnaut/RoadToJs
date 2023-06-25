f092a0a02fa8814d3a62b2d3b50683e3694c4795
omlnaut <oneironaut.oml@gmail.com>
2023-06-25 12:18:10 +0000

    finished initial tutorial

---
+++ js-basics/index.js
@@ -0,0 +1,12 @@
+// Performing a task
+function greet(name, lastName='LastName'){
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+// Calculating a value
+function square(number) {
+    return number*number
+}
+
+let number = square(2);
+console.log(number);
